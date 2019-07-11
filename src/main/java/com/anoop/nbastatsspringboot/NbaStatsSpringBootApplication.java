1d3c7a6c0a472dccb2ec7faf541c6b74b51b473d
Anoop Ravella <anoopr@umich.edu>
2019-07-10 22:33:46 -0400

    added initial files from spring initializr

---
+++ src/main/java/com/anoop/nbastatsspringboot/NbaStatsSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.anoop.nbastatsspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaStatsSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbaStatsSpringBootApplication.class, args);
+	}
+
+}
